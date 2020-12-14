b77eb134ebc952da9719f743743ae69f7bfa4a96
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-12-14 14:05:03 -0500

    start android-database-sqlcipher-evplus-native-driver-build-free

--- android-database-sqlcipher/src/main/cpp/Android.mk
+++ android-database-sqlcipher/src/main/cpp/Android.mk
@@ -3,14 +3,16 @@ MY_PATH := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 LOCAL_PATH := $(MY_PATH)
 
+C_SRC_ROOT := $(LOCAL_PATH)/../external/android-sqlite-evplus-ext-native-driver-free
+
 LOCAL_CFLAGS +=  $(SQLCIPHER_CFLAGS) $(SQLCIPHER_OTHER_CFLAGS)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_LDLIBS := -llog
 LOCAL_LDFLAGS += -L$(ANDROID_NATIVE_ROOT_DIR)/$(TARGET_ARCH_ABI) -fuse-ld=bfd
 LOCAL_STATIC_LIBRARIES += static-libcrypto
-LOCAL_MODULE    := sqlcipher-core
-LOCAL_SRC_FILES := sqlite3.c \
-	# end of LOCAL_SRC_FILES
+LOCAL_MODULE    := sqlc-evplus-native-driver
+# includes the SQLite amalgamation:
+LOCAL_SRC_FILES := $(C_SRC_ROOT)/native/sqlc_all.c
 
 include $(BUILD_SHARED_LIBRARY)
 
