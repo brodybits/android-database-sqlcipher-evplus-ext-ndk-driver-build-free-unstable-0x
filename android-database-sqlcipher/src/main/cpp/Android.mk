a9cbc4cc086049f0c10f1f63440c424d43d5473c
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-08-18 12:51:23 -0400

    start android-database-sqlcipher-evplus-ext-ndk-driver-build-free

--- android-database-sqlcipher/src/main/cpp/Android.mk
+++ android-database-sqlcipher/src/main/cpp/Android.mk
@@ -3,14 +3,19 @@ MY_PATH := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 LOCAL_PATH := $(MY_PATH)
 
+C_SRC_ROOT := $(LOCAL_PATH)/../external/android-sqlite-evplus-ndk-driver-free
+
 LOCAL_CFLAGS +=  $(SQLCIPHER_CFLAGS) $(SQLCIPHER_OTHER_CFLAGS)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_LDLIBS := -llog
 LOCAL_LDFLAGS += -L$(ANDROID_NATIVE_ROOT_DIR)/$(TARGET_ARCH_ABI) -fuse-ld=bfd
 LOCAL_STATIC_LIBRARIES += static-libcrypto
-LOCAL_MODULE    := sqlcipher-ndk-core
-LOCAL_SRC_FILES := sqlite3.c \
-	# end of LOCAL_SRC_FILES
+LOCAL_MODULE    := sqlc-evplus-ndk-driver
+# includes the SQLite amalgamation etc:
+LOCAL_SRC_FILES := $(C_SRC_ROOT)/native/sqlc_all.c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../external/libb64-core
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../external/sqlite3-base64
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../external/sqlite3-regexp-cached
 
 include $(BUILD_SHARED_LIBRARY)
 
